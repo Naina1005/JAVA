ed9c675de3388581b9e9d46a302a18ac49b9d84c
NainaTyagi <91248921+Naina1005@users.noreply.github.com>
2024-09-08 12:21:00 +0530

    Add files via upload

---
+++ 14.Characters-J/src/Main.java
@@ -0,0 +1,10 @@
+//Problem Statement : To try increment and decrement operators with characters
+public class Main{
+    public static void main(String[] args) {
+        char a = 'B';
+        //incrementing character variable a
+        System.out.println("character a on incrementing becomes : "+ ++a);
+        System.out.println("character a on decrementing becomes : "+ --a);
+
+    }
+}
