45ff8ef48cb69da3f0909f2a40f2da264da43495
NainaTyagi <91248921+Naina1005@users.noreply.github.com>
2024-09-10 08:58:20 +0530

    Add files via upload

---
+++ 22.Formatting-J/src/Main.java
@@ -0,0 +1,9 @@
+//Problem statement : Formatting a string using escape characters
+public class Main{
+    public static void main(String[] args) {
+        String s = "Hello! Have a good day .";
+        System.out.println("printing string as it is  :"+s);
+        System.out.println("Formatting the string using escape characters : Hello\nHave a good day .");
+
+    }
+}
