bc111d09d943aeb6dff3d1dd3c735478e3fef67e
NainaTyagi <91248921+Naina1005@users.noreply.github.com>
2024-09-12 17:27:47 +0530

    Add files via upload

---
+++ 34.EvenSum-J/src/Main.java
@@ -0,0 +1,18 @@
+//Problem statement : Sum of first n even numbers
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n ;
+        System.out.println("Enter a number: ");
+        n = sc.nextInt();
+        int i = 1;
+        int sum = 0;
+        while(i <= n)
+        {
+           sum = sum + i;
+           i++;
+        }
+        System.out.println(sum);
+    }
+}
