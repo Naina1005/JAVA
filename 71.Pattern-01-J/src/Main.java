dded0c5457eea34203eec4c30e0f4d6c91bf2ba5
NainaTyagi <91248921+Naina1005@users.noreply.github.com>
2024-10-11 22:31:35 +0530

    Add files via upload

---
+++ 71.Pattern-01-J/src/Main.java
@@ -0,0 +1,24 @@
+//Problem statement : To print the following pattern
+/*
+
+*
+**
+***
+****
+*****
+
+*/
+
+//Main class
+public class Main{
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
